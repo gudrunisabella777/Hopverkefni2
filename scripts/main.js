cce7b7647efe20889651b10027c4444e4c3b80d9
mariaorra <mao44@hi.is>
2022-11-19 17:27:49 +0000

    sýna gögn
    byrjaði að reyna að sýna dagsetningu, spurning hvernig hægt að breyta gögnunum

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,12 @@ fetch('../data/events.json')
     .catch(function (err) {
         console.log(err);
     });
+
+function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = JSON.stringify(data[i].start);
+        mainContainer.appendChild(div);
+    }
+}
