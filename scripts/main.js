727f7c8b226c9ef25353274e2d2f2b0a0f0e7127
mariaorra <mao44@hi.is>
2022-11-19 16:40:30 +0000

    main
    smá breyting í index, annars reyna að finna út úr main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
-fetch('./data/events.json')
+/*fetch('../data/events.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })*/
+
+
+/*fetch('../data/events.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((events) => {
+        console.log(events);
+    });*/
+
+fetch('../data/events.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
