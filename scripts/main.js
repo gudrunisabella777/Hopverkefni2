71b8f1a9bbbc3879231db403dd29dfee4f0311fb
Guðrún Ísabella Kjartansdóttir <gik10@hi.is>
2022-11-23 13:46:16 +0000

    suprise merge conflict

--- scripts/main.js
+++ scripts/main.js
@@ -96,9 +96,9 @@ function search_Viðburður() {
 }
 
 //Reyna að búa til lista (nota pageing)
-const list_items = {
+/*const list_items = {
     "Event 1-30"
-};
+};*/
 
 const list_element = document.getElementById('list');
 const pagination_element = document.getElementById('pagination');
@@ -142,8 +142,8 @@ function Pagination(page) {
     if(current_page == page) button.classList.add('active');
  
  
-    button.addEventListener('click', function ())
-    return button;
+    /*button.addEventListener('click', function ())
+    return button;*/
     
 }
 
